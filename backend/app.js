d1eedabb14723eded7e3ccc59b49cc586e38ab30
Guoao Wei <guoao.wei@outlook.com>
2022-11-21 22:35:13 -0800

    Modify app to integrate animal router, add .env file

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
+require("dotenv").config();
 
 // Import routers
 // var indexRouter = require('./routes/index');
-var dbRouter = require('./routes/db');
+// var dbRouter = require('./routes/db');
+const animalRouter = require("./routes/animalRoutes.js");
+
+mongoose.connect(process.env.pawsURI, {useNewUrlParser: true})
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err))
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,8 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/api/db', dbRouter);
+// app.use('/', indexRouter);
+// app.use('/api/db', dbRouter);
+app.use(animalRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
