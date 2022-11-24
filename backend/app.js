f07924db6b87ce6fdfbd6077cdc6702291e346bc
Guoao Wei <guoao.wei@outlook.com>
2022-11-23 21:45:01 -0800

    Add backend default router

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 // app.use('/api/db', dbRouter);
 app.use(animalRouter);
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + "/public", 'index.html')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
