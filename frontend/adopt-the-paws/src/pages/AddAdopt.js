81946c6f354b8b1369c46bac809f34d92d752542
Guoao Wei <guoao.wei@outlook.com>
2022-11-23 18:01:11 -0800

    Delete unused imports in AddAdopt page

--- frontend/adopt-the-paws/src/pages/AddAdopt.js
+++ frontend/adopt-the-paws/src/pages/AddAdopt.js
@@ -1,23 +1,9 @@
 import React from 'react'
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import SelectGender from "../components/SelectGender";
-import SelectBreed from "../components/SelectBreed";
-import SelectColor from "../components/SelectColor";
-import SelectCoatLength from "../components/SelectCoatLength";
-import SelectGoodWith from "../components/SelectGoodWith";
 import AddForm from '../components/AddForm';
 
-import ProductCategories from '../views/ProductCategories';
-import ProductSmokingHero from '../views/ProductSmokingHero';
 import AppFooter from '../views/AppFooter';
-import ProductHero from '../views/ProductHero';
 import ProductCTA from '../views/ProductCTA'
-import ProductHowItWorks from '../views/ProductHowItWorks';
-import GridCard from '../views/GridCard'
 import AppAppBar from '../views/AppAppBar';
-import { Navigate } from 'react-router-dom';
 
 
 export default function AddAdopt() {
